6e56d3260d25742dc0329c55c7d06784fab83586
Amrut <amrutpanda7789@gmail.com>
2020-06-09 23:56:43 +0530

    Snake and Ladder multiplayer game is added

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 #@Author: Amrut
-#Snake N Ladder Use Case 6
+#Snake N Ladder Use Case 7
 
 echo "******Welcome to SnakeNLadder Game******"
 
@@ -12,9 +12,9 @@ LADDER=2
 WINNING_POSITION=100
 FLAG=true
 
-player=1
 CurrentPosition=$START_POSITION
 numOfRolls=0
+checkTurn=0
 
 declare -A positions
 
@@ -44,6 +44,7 @@ function rollDice() {
 				;;
 		esac
 }
+
 function winningPosition() {
 
 		while [ $FLAG ]
@@ -62,8 +63,37 @@ function winningPosition() {
 			fi
 		done
 }
-
 winningPosition
 echo "${!positions[@]} : ${positions[@]}"
 echo "Total Dice Rolls to reach the winning position is : $numOfRolls"
 echo "**Player reaches to the winning position :: $CurrentPosition**"
+
+function multiPlayer()
+{
+   while [ $FLAG ]
+   do
+      if [ $(( checkTurn%2 )) -eq 0 ]
+      then
+      rollDice
+      (( checkTurn++ ))
+         if [ $CurrentPosition -eq $WINNING_POSITION ]
+         then
+            echo "Player 1 Won"
+            break
+         fi
+       fi
+
+       if [ $(( checkTurn%2 )) -eq 1 ]
+       then
+       rollDice
+       (( checkTurn++ ))
+          if [ $CurrentPosition -eq $WINNING_POSITION ]
+          then
+             echo "Player 2 Won"
+             break
+          fi
+       fi
+   done
+}
+
+multiPlayer
